1120c567f7f573850abf70e4a3031ee10e462f79
vseriousv <gavrilovskirill@gmail.com>
2019-11-18 21:17:56 +0700

    add captcha

--- components/ContactsFeedback/ContactsFeedback.js
+++ components/ContactsFeedback/ContactsFeedback.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
+import Recaptcha from "react-recaptcha";
+
+import './ContactsFeedback.less';
 
 export default class ContactsFeedback extends Component{
+
+	verifyCallback = ()  => {
+		console.log('Done!!!!');
+	};
+
   render() {
     return(
-		<div className="feedback mb-5">		
+		<div className="feedback mb-5">
 			<div className="container">
 				<div className="row">
 					<div className="col-12 col-sm-6 mb-3">
 						<div className="box_sms">
-							<textarea type="text" name="" className="input_sms" placeholder="Сообщение"></textarea> 
-						</div> 
-					</div>	
+							<textarea type="text" name="" className="input_sms" placeholder="Сообщение"></textarea>
+						</div>
+					</div>
 					<div className="col-12 col-sm-6 mb-3">
 						<div className="box_data">
 							<input type="text" name="" className="data_client text-center" placeholder="Имя" />
@@ -19,18 +27,17 @@ export default class ContactsFeedback extends Component{
 					</div>
 					<div className="col-12 col-sm-6 security offset-sm-6 justify-content-center justify-content-sm-start">
 						<div className="capcha_box mb-4">
-							<div className="form-check">
-								<input className="check-input" name="" type="checkbox" value="" id="defaultCheck1" />
-								 <label className="form-check-label h_14 myblack" for="defaultCheck1">
-									 Я не робот
-								 </label>
-								 <p className="h_14_app myblack m-0">КАПЧА</p>
-							</div>
+							<Recaptcha
+								sitekey="6LeHA7oUAAAAABmSgN0MbTrSAjOSonBbqeaS43l3"
+								render="explicit"
+								className="captchaReact"
+								onloadCallback={this.verifyCallback}
+							/>
 						</div>
 						<div className="security_box mb-4">
 							<input className="check_cec" name="" type="checkbox" value="" id="defaultCheck2" />
 							 <label className="consent h_14 myblack" for="defaultCheck2">
-								 Я даю своё согласие на обработку персональных данных в соответствии с 
+								 Я даю своё согласие на обработку персональных данных в соответствии с
                                     <a href="" className="site_rules">правилами сайта</a>
 							 </label>
 						</div>
